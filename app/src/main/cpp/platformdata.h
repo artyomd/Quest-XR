1c671f84eb3ecfd71b84154bbefb0cb0c725e5ca
Artyom Dangizyan <artyom.dangizyan@gmail.com>
2020-11-16 21:11:32 +0400

    add OpenXR demo for quest2

---
+++ app/src/main/cpp/platformdata.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2017-2020 The Khronos Group Inc
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+struct PlatformData {
+    void* applicationVM;
+    void* applicationActivity;
+};
---
+++ app/src/main/vendor/openxr/openxr_oculus.h
@@ -0,0 +1,60 @@
+/************************************************************************************
+
+Filename    :   openxr_oculus.h
+Content     :   Oculus OpenXR Extensions.
+Language    :   C99
+
+Copyright   :   Copyright (c) Facebook Technologies, LLC and its affiliates. All rights reserved.
+
+*************************************************************************************/
+
+#ifndef OPENXR_OCULUS_H_
+#define OPENXR_OCULUS_H_ 1
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+// Extension 89
+#ifndef XR_KHR_loader_init
+#define XR_KHR_loader_init 1
+#define XR_KHR_loader_init_SPEC_VERSION 1
+#define XR_KHR_LOADER_INIT_EXTENSION_NAME "XR_KHR_loader_init"
+typedef struct XR_MAY_ALIAS XrLoaderInitInfoBaseHeaderKHR {
+    XrStructureType type;
+    const void* XR_MAY_ALIAS next;
+} XrLoaderInitInfoBaseHeaderKHR;
+
+typedef XrResult(XRAPI_PTR* PFN_xrInitializeLoaderKHR)(
+    const XrLoaderInitInfoBaseHeaderKHR* loaderInitInfo);
+
+#ifndef XR_NO_PROTOTYPES
+XRAPI_ATTR XrResult XRAPI_CALL
+xrInitializeLoaderKHR(const XrLoaderInitInfoBaseHeaderKHR* loaderInitInfo);
+#endif
+
+#endif // XR_KHR_loader_init
+
+// Extension 90
+#ifndef XR_KHR_loader_init_android
+#ifdef XR_USE_PLATFORM_ANDROID
+
+#define XR_KHR_loader_init_android 1
+#define XR_KHR_loader_init_android_SPEC_VERSION 1
+#define XR_KHR_LOADER_INIT_ANDROID_EXTENSION_NAME "XR_KHR_loader_init_android"
+typedef struct XrLoaderInitInfoAndroidKHR {
+    XrStructureType type;
+    const void* XR_MAY_ALIAS next;
+    void* XR_MAY_ALIAS applicationVM;
+    void* XR_MAY_ALIAS applicationContext;
+} XrLoaderInitInfoAndroidKHR;
+
+static const XrStructureType XR_TYPE_LOADER_INIT_INFO_ANDROID_KHR = (XrStructureType)1000089000;
+#endif /* XR_USE_PLATFORM_ANDROID */
+#endif // XR_KHR_loader_init_android
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // OPENXR_OCULUS_H_
